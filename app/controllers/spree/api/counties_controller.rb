417ca0ae2808771bc59816fa49331222e56bea4f
Rahul <rahul@bluebash.co>
2021-04-05 15:51:06 +0530

    fixed issue for skip before filter callback

--- app/controllers/spree/api/counties_controller.rb
+++ app/controllers/spree/api/counties_controller.rb
@@ -1,21 +1,20 @@
 module Spree
   module Api
     class CountiesController < Spree::Api::BaseController
-      skip_before_filter :set_expiry
-      skip_before_filter :check_for_user_or_api_key
-      skip_before_filter :authenticate_user
+      # skip_before_action :set_expiry
+      # skip_before_action :check_for_user_or_api_key
+      skip_before_action :authenticate_user
 
       def index
         @counties = scope.ransack(params[:q]).result.
                     includes(:state).order('name ASC')
 
-        if params[:page] || params[:per_page]
-          @counties = @counties.page(params[:page]).per(params[:per_page])
-        end
+        @counties = @counties.page(params[:page]).per(params[:per_page]) if params[:page] || params[:per_page]
 
         county = @counties.last
         if stale?(county)
-          respond_with(@counties)
+          # respond_with(@counties)
+          render json: { counties: @counties }
         end
       end
 
@@ -25,14 +24,15 @@ module Spree
       end
 
       private
-        def scope
-          if params[:state_id]
-            @state = Spree::State.accessible_by(current_ability, :read).find(params[:state_id])
-            return @state.counties.accessible_by(current_ability, :read)
-          else
-            return Spree::County.accessible_by(current_ability, :read)
-          end
+
+      def scope
+        if params[:state_id]
+          @state = Spree::State.accessible_by(current_ability, :read).find(params[:state_id])
+          @state.counties.accessible_by(current_ability, :read)
+        else
+          Spree::County.accessible_by(current_ability, :read)
         end
+      end
     end
   end
 end
