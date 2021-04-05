c20ac81915f78aa88f8e92bd626eab1bb4f9da07
Rahul <rahul@bluebash.co>
2021-04-05 15:46:44 +0530

    fixed override issue for state decorator

--- app/models/spree/state_decorator.rb
+++ app/models/spree/state_decorator.rb
@@ -1,3 +1,7 @@
-Spree::State.class_eval do
-  has_many :counties, -> { order('name ASC') }, dependent: :destroy
+module Spree::StateDecorator
+  def self.prepended(base)
+    base.has_many :counties, -> { order('name ASC') }, dependent: :destroy
+  end
+
+  Spree::State.prepend self
 end
